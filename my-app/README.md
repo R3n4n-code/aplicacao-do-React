f5374f1731b3dc1cf4003c585f48852f22194549
Renan Mezzacapa <renanrusefqw@gmail.com>
2026-05-14 20:23:52 -0300

    ajustes

--- my-app/README.md
+++ my-app/README.md
@@ -1,16 +1,13 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Este modelo fornece uma configuração mínima para fazer o React funcionar no Vite com HMR e algumas outras funcionalidades.
 
-Currently, two official plugins are available:
+Atualmente, dois plugins oficiais estão disponíveis:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Oxc](https://oxc.rs)
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/)
 
-## React Compiler
+# Comando para rodar
 
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+cd my-app
+npm run dev
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,41 +2,72 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import Card from './card'
-import "./app.css"
+import Card from './Componentes/ProdutoCard'
+import "./Visual/app.css"
 
 function App() {
   const [loading, setLoading] = useState(false)
   const [titulo, setTitulo] = useState("")
   const [descricao, setDescricao] = useState("")
-  const [valor, setValor] = useState("") 
+  const [valor, setValor] = useState("")
   const [cards, setCards] = useState([])
-   
-  function adicionarCard() {
-  
-    const novoCard = { 
-    id: Date.now(),
-    titulo: titulo,
-    descricao: descricao,
-    valor: valor,
-    imagem: "https://placehold.co/600x400",
-  }
-  setLoading(true)
+
+  useEffect(() => {
+    setLoading(true)
     setTimeout(() => {
-  
-   setCards([... cards,novoCard])  
-    { 
-       setTitulo("")
-       setDescricao("")
-       setValor("")
 
-          } 
-setLoading(false)
-  }, 2000)
+      const dadosSimulados = [{
+        id: 1,
+        titulo: "Hambúrguer",
+        descricao: "Muito gostoso",
+        valor: 50,
+        imagem: "https://placehold.co/600x400"
 
-}          
+      },
+
+      {
+        id: 2,
+        titulo: "Pizza",
+        descricao: "Queijo extra",
+        valor: 80,
+        imagem: "https://placehold.co/600x400"
+      }]
+      setCards(dadosSimulados)
+      setLoading(false)
+
+    }, 3000)
+  }, [])
+
+
+
+  function adicionarCard() {
+    if (titulo === "" || descricao === "" || valor === "") {
+      alert("prencha todos os campos!!")
+      return
+    }
+    const novoCard = {
+      id: Date.now(),
+      titulo: titulo,
+      descricao: descricao,
+      valor: valor,
+      imagem: "https://placehold.co/600x400",
+    }
+    setLoading(true)
+    setTimeout(() => {
+
+      setCards([...cards, novoCard])
+
+      setTitulo("")
+      setDescricao("")
+      setValor("")
+
+
+      setLoading(false)
+    }, 2000)
+
+  }
   function remover(id) {
-     const novosCards = cards.filter((card) => {
+    const novosCards = cards.filter((card) => {
       return card.id !== id
     })
 
@@ -69,26 +100,31 @@ setLoading(false)
             onChange={(e) => setValor(e.target.value)}
           />
           <button onClick={adicionarCard}>Adicionar</button>
- 
+
         </div>
-      </div>
-    
-      <div className='container'>
-        {loading && <h1>Carregando</h1>}
-        {cards.map((card) => (
-          <Card
-            key={card.id}
-            className={"card"}
-            titulo={card.titulo}
-            
-            descricao={card.descricao}
-            imagem={card.imagem}
-            botao={"Remover"}
-            remover={() => remover(card.id)}
-            valor={card.valor}
-          />
+
+        <div className='loading'>
+          {loading && <h1>Carregando...</h1>}
+        </div>
+
+        <div className='container'>
+
+
+
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              className={"card"}
+              titulo={card.titulo}
+
+              descricao={card.descricao}
+              imagem={card.imagem}
+              botao={"Remover"}
+              remover={() => remover(card.id)}
+              valor={card.valor}
+            />
           ))}
-          
+        </div>
       </div>
 
     </div>
